29c006ee0fca5d1d5fb19a349033f0848fbefef6
Massey <masseybr@cs.unc.edu>
2026-01-22 10:39:29 -0500

    Swapped to treelite4

--- Python_to_C_pipeline.py
+++ Python_to_C_pipeline.py
@@ -1,15 +1,19 @@
 import joblib
 import treelite
 import treelite.sklearn
+import tl2cgen
 
 rf = joblib.load("randforest_first8.joblib")
-tl = treelite.sklearn.import_model(rf)
 
-# Build a shared library you can link/use from C
-tl.export_lib(
-    libpath="rf_first8.so",
+# Treelite: load model into Treelite representation
+model = treelite.sklearn.import_model(rf)
+
+# TL2cgen: compile/export to a shared library
+tl2cgen.export_lib(
+    model,
     toolchain="gcc",
-    verbose=True
+    libpath="rf_first8.so",
+    params={},      # can be left empty
 )
 
 print("Built rf_first8.so")
