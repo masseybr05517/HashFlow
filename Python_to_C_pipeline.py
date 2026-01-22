410481b0d3089d371c8483a8b6990cfd2ed4f615
Massey <masseybr@cs.unc.edu>
2026-01-22 10:18:54 -0500

    Adding print statements

--- Python_to_C_pipeline.py
+++ Python_to_C_pipeline.py
@@ -2,7 +2,8 @@ import joblib
 import m2cgen as m2c
 
 rf = joblib.load("randforest_first8.joblib")
-
+print("Loaded object type:", type(rf))
+print("Loaded object repr:", rf)
 c_code = m2c.export_to_c(rf)
 
 with open("rf_first8_model.c", "w") as f:
